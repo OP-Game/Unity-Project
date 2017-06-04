cdd6c3e08cb2ed8ab9102705aede0ee5871973fa
TheGeffenM <thegeffenm18@gmail.com>
2017-06-04 12:50:01 -0400

    UI Updates
    Added health bar UI elements
    Started refining weapon code

--- OP Game/Assets/Scripts/PlayerManager.cs
+++ OP Game/Assets/Scripts/PlayerManager.cs
@@ -13,6 +13,13 @@ public class PlayerManager : MonoBehaviour
     private GameObject bulletPrefab;
     public string equippedWep;
     public bool isAiming;
+
+    //reloadTime - seconds it takes to reload
+    //shotDelay - time in seconds between being able to fire again
+    private float reloadTime, shotDelay;
+
+    //startTime - Time that the fire key is pressed
+    //power - draw power for bows
     private float startTime, power;
 
 
@@ -31,6 +38,8 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Checks to see if the FOV is just under 80, if so it snaps to 80. This is to prevent FOV from lerping through infinity without reaching 80.
         if(this.GetComponentInChildren<Camera>().fieldOfView >= 79.8f && !this.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().isSprinting && !isAiming)
         {
             this.GetComponentInChildren<Camera>().fieldOfView = 80f;
@@ -40,6 +49,10 @@ public class PlayerManager : MonoBehaviour
         //Set aiming to true, starts the FOV coroutin
         if (equippedWep == "Bow" && Input.GetButtonDown("Fire1"))            
         {
+
+            //since we are drawing the bow, set Aiming to true
+            //capture the start time of the bow draw
+            //stop all FOV coroutines and start the aimFOV routine to zoom the camera
             isAiming = true;
             startTime = Time.time;
             StopCoroutine(aimFOV());
@@ -50,6 +63,8 @@ public class PlayerManager : MonoBehaviour
         //When you RELEASE the "Fire1" input, get the difference in time since you pressed fire, then multiply it by 125 up to a max of 250, which is then used as the force for the arrow.
         if (equippedWep == "Bow" && Input.GetButtonUp("Fire1"))              
         {
+            //since we released the bow, set Aiming to false
+            //stop the aiming Coroutines, then start the returnFOV coroutine to return FOV to 80
             isAiming = false;
             StopCoroutine(aimFOV());
             StopCoroutine(returnFOV());
@@ -126,19 +141,27 @@ public class PlayerManager : MonoBehaviour
     void Equip(string weapon)
     {
 
+        //gets the currentWep which the weapon that is attached to this transform under "FirstPersonCharacter" + equippedWep. Example: FirstPersonCharacter/Pistol
+        //       **Always name weapons their EXACT name**
         var currentWep = this.transform.Find("FirstPersonCharacter/" + equippedWep).gameObject;
 
+        //Deactivates the current Weapon
         currentWep.SetActive(false);
 
-
+        //sets the equippedWeapon to the Weapon that we just equipped
+        //sets the current weapon to the weapon in the patch defined
+        //activates the currentWeapon
         equippedWep = weapon;
         currentWep = this.transform.Find("FirstPersonCharacter/" + equippedWep).gameObject;
         currentWep.SetActive(true);
 
+        //gets the shotStart transform of the new weapon under the "FirstPersonCharacter/WEAPONNAME/" path. Example "FirstPersonCharacter/Pistol/shotStart"
+        //      **ALWAYS NAME SHOTSTART: shotStart**
         shotStart = this.transform.Find("FirstPersonCharacter/" + weapon + "/shotStart").gameObject;
 
     }
 
+    //Co-routine used to perform an FOV zoom when ADS
     IEnumerator aimFOV()
     {
         while (isAiming)
@@ -148,6 +171,8 @@ public class PlayerManager : MonoBehaviour
         }
 
     }
+
+    //Co-routine used to return the FOV to normal after ADS
     IEnumerator returnFOV()
     {
         while(!isAiming)
---
+++ OP Game/Assets/Scripts/WeaponScripts/Pistol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour {
+
+    /*
+    needsReload - does this weapon need to be reloaded
+    reloadTime - time in seconds it takes to reload
+    shotDelay - time in seconds between consecutive shots
+    maxAmmo - total ammo count
+    clipSize - number of rounds in a single clip
+    fovZoomAmt - amount of FOV zoom to be applied when ADS
+    projectilePrefab - string name of the projectile to be instantiated when fired
+    power - velocity amount to be added to the projectile when fired
+    */
+
+    public bool needsReload = false;
+    public float reloadTime = 0;
+    public float shotDelay = 1f;
+    public int maxAmmo = 100;
+    public int clipSize = 100;
+    public float fovZoomAmt = 15f;
+    public string projectilePrefab = "Tri__Bullet";
+    public float shotPower = 200;
+	
+}
