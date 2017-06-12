3603598fa47322c09044ee940973ef99c87a23bb
saladofrob <rob@robscheld.com>
2017-06-12 19:17:21 -0400

    dfgsdfgsdf

--- OP Game/Assets/UIManager.cs
+++ OP Game/Assets/UIManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UIManager : MonoBehaviour {
     private bool isShowing = false;
     private GameObject menu;
+    
 
     void QuitApplication()
     {
@@ -24,6 +25,7 @@ public class UIManager : MonoBehaviour {
         {
             isShowing = !isShowing;
         }
+
     }
 
 }
